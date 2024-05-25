9eca3a445a8cac9aca30e0c1d49dcf505afd4716
墨离 <165390418+molikai-work@users.noreply.github.com>
2024-05-25 10:15:05 +0800

    Update [id].js

--- functions/[id].js
+++ functions/[id].js
@@ -42,8 +42,6 @@ export async function onRequestGet(context) {
         <!-- Clarity tracking code for https://c1n.top/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "m0fkugt98p");</script><!-- End Clarity tracking code for https://c1n.top/ -->
     `
 
-//    const body =``
-
     const adminEmail =`info@molikai.work`
 
     const noscript =`<noscript>
